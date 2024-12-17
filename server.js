b73dfb9704449397e5810cef9b605e150e0255ca
heeral-makwana <makwanahiral60@gmail.com>
2024-12-17 20:04:43 +0530

    Added cors

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 const cookieParser = require('cookie-parser')
 const fileUpload = require('express-fileupload')
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 app.use(express.json())             
 app.use(cookieParser())
 app.use(fileUpload({
